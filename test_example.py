8184f3eb464d016901bb7441c2fc093e655402b7
Lucas Almeida <72702201+zzzluquinhas@users.noreply.github.com>
2025-10-01 11:30:46 -0300

    Commit 6: Executando Todos os Testes

--- test_example.py
+++ test_example.py
@@ -1,9 +1,19 @@
 from test_loader_test import TestLoaderTest
 from test_loader import TestLoader
 from test_runner import TestRunner
+from test_suite_test import TestSuiteTest
+from testcase_test import TestCaseTest
+from test_suite import TestSuite
 
 loader = TestLoader()
-suite = loader.make_suite(TestLoaderTest)
+test_case_suite = loader.make_suite(TestCaseTest)
+test_suite_suite = loader.make_suite(TestSuiteTest)
+test_load_suite = loader.make_suite(TestLoaderTest)
+
+suite = TestSuite()
+suite.add_test(test_case_suite)
+suite.add_test(test_suite_suite)
+suite.add_test(test_load_suite)
 
 runner = TestRunner()
 runner.run(suite)
