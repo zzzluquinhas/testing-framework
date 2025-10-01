2e796a6f3175afb01c5e097b72f4f645e536de01
Lucas Almeida <72702201+zzzluquinhas@users.noreply.github.com>
2025-10-01 11:33:46 -0300

    Commit 7: Comandos Assert

--- testcase.py
+++ testcase.py
@@ -21,3 +21,23 @@ class TestCase:
 
 	def tear_down(self):
 		pass
+
+	def assert_equal(self, first, second):
+		if first != second:
+			msg = f'{first} != {second}'
+			raise AssertionError(msg)
+		
+	def assert_true(self, expr):
+		if not expr:
+			msg = f'{expr} is not true'
+			raise AssertionError(msg)
+
+	def assert_false(self, expr):
+		if expr:
+			msg = f'{expr} is not false'
+			raise AssertionError(msg)
+	
+	def assert_in(self, member, container):
+		if member not in container:
+			msg = f'{member} not found in {container}'
+			raise AssertionError(msg)
--- testcase_test.py
+++ testcase_test.py
@@ -51,3 +51,27 @@ class TestCaseTest(TestCase):
         spy = TestSpy('test_method')
         spy.run(self.result)
         assert spy.log == "set_up test_method tear_down"
+        
+    def test_assert_true(self):
+        self.assert_true(True)
+
+    def test_assert_false(self):
+        self.assert_false(False)
+
+    def test_assert_equal(self):
+        self.assert_equal("", "")
+        self.assert_equal("foo", "foo")
+        self.assert_equal([], [])
+        self.assert_equal(['foo'], ['foo'])
+        self.assert_equal((), ())
+        self.assert_equal(('foo',), ('foo',))
+        self.assert_equal({}, {})
+        self.assert_equal({'foo'}, {'foo'})
+
+    def test_assert_in(self):
+        animals = {'monkey': 'banana', 'cow': 'grass', 'seal': 'fish'}
+
+        self.assert_in('a', 'abc')
+        self.assert_in('foo', ['foo'])
+        self.assert_in(1, [1, 2, 3])
+        self.assert_in('monkey', animals)
